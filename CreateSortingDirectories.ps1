0c819d6ce3a61c856b5d213c23b77198e38547e1
Marcus Kern <skyacem@hotmail.com>
2020-07-24 09:44:48 -0500

    added create directory scripts

---
+++ CreateSortingDirectories.ps1
@@ -0,0 +1,33 @@
+
+[System.Collections.Generic.List[String]]$CharDirectories = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ#'.ToCharArray()
+[System.Collections.Generic.List[String]]$NamedDirectories = "Adult Only", "Compliation Disks", "# Unsorted";
+[System.Collections.Generic.List[String]]$FullDirectories = New-Object "System.Collections.Generic.List[String]";
+
+foreach ($dirName in $CharDirectories) { $FullDirectories.Add($dirName) }
+foreach ($dirName in $NamedDirectories) { $FullDirectories.Add($dirName) }
+
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "", Justification="Just an example")]
+function Create-SortDirectories {
+    [CmdletBinding()]
+    param (
+        [System.Collections.Generic.List[String]]$directoriesToCreate 
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        $directoriesToCreate.Clear();
+        foreach ($d in $directoriesToCreate) {
+            New-Item $d
+        }
+    }
+    
+    end {
+        
+    }
+}
+
+
+
